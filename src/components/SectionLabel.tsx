30e55fa7845ae0f8084f85825271c8854b6c783d
Grace Ge <NA>
2025-11-05 19:40:01 -0800

    Increase SectionLabel base size to 12px

--- src/components/SectionLabel.tsx
+++ src/components/SectionLabel.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 interface SectionLabelProps extends React.HTMLAttributes<HTMLParagraphElement> {}
 
-const baseClasses = 'text-[11px] sm:text-[12px] font-normal text-[#CAC4D0] mb-1.5 sm:mb-2 tracking-widest'
+const baseClasses = 'text-[12px] sm:text-[13px] font-normal text-[#CAC4D0] mb-1.5 sm:mb-2 tracking-widest uppercase'
 
 const SectionLabel = React.forwardRef<HTMLParagraphElement, SectionLabelProps>(
   ({ className = '', style, children, ...props }, ref) => {
